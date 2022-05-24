1353d4318dddb3baa300a00e2904306e5efa93ec
hatinurami <hati-nurami@yandex.ru>
2022-05-24 15:48:02 +0300

    --фильтр студентов (доделать)

--- frames/Zavuch.xaml.cs
+++ frames/Zavuch.xaml.cs
@@ -47,26 +47,114 @@ namespace elj.frames
 
         private void Filtr()
         {
-            if (cb_Group.SelectedItem != null)
+
+
+            if (cb_Group.SelectedItem != null || cb_Kurs.SelectedItem != null || cb_Status.SelectedItem != null)
             {
-                cb_Kurs.IsEnabled = false;
-                int filtr_gr = cb_Group.SelectedIndex +1;
-                infoGridStudents.ItemsSource = context.Student.Where(i => i.studGroup == filtr_gr).ToList();
-                if (cb_Status.SelectedItem != null)
+
+                int filtr_gr = cb_Group.SelectedItem != null ? cb_Group.SelectedIndex + 1 : 0 ;
+                int filtr_krs = cb_Kurs.SelectedItem != null ? cb_Kurs.SelectedIndex + 1 : 0 ;
+                string filtr_statis = cb_Status.SelectedItem != null ? cb_Status.SelectedValue.ToString() : null;
+
+                if (filtr_gr != 0)
                 {
-                    string filtr_statis = cb_Status.SelectedValue.ToString();
                     infoGridStudents.ItemsSource = context.Student.
-                        Where(i => i.studGroup == filtr_gr && i.status == filtr_statis).ToList();
+                      Where(i => i.studGroup == filtr_gr);
+                    if (filtr_krs != 0)
+                    {
+                        infoGridStudents.ItemsSource = context.Student.
+                      Where(i => i.StudGroup1.course == filtr_krs && i.studGroup == filtr_gr);
+                        if (filtr_statis != null)
+                        {
+                            infoGridStudents.ItemsSource = context.Student.
+                      Where(i => i.StudGroup1.course == filtr_krs && i.studGroup == filtr_gr && i.status == filtr_statis);
+                        }
+                    }
                 }
+                
+
+
+
+
+                //infoGridStudents.ItemsSource = context.Student.
+                //      Where(i => i.studGroup == filtr_gr && i.status == filtr_statis && i.StudGroup1.course == filtr_krs
+                //      || i.studGroup == filtr_gr && i.status == filtr_statis ||
+                //      i.status == filtr_statis && i.StudGroup1.course == filtr_krs|| i.studGroup == filtr_gr || i.StudGroup1.course == filtr_krs || i.status == filtr_statis
+                //      ).ToList();
 
             }
 
 
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //if (cb_Group.SelectedItem != null)
+            //{
+
+            //  int filtr_gr = cb_Group.SelectedIndex +1;
+            //  infoGridStudents.ItemsSource = context.Student.Where(i => i.studGroup == filtr_gr).ToList();
+            //  if (cb_Status.SelectedItem != null)
+            //  {
+            //      string filtr_statis = cb_Status.SelectedValue.ToString();
+            //      infoGridStudents.ItemsSource = context.Student.
+            //          Where(i => i.studGroup == filtr_gr && i.status == filtr_statis).ToList();
+            //  }
+
+            //}
+            //if (cb_Kurs.SelectedItem != null)
+            //{
+
+            //  int filtr_krs = cb_Kurs.SelectedIndex + 1;
+            //  infoGridStudents.ItemsSource = context.Student.Where(i => i.StudGroup1.course == filtr_krs).ToList();
+
+            //  if (cb_Status.SelectedItem != null)
+            //  {
+            //      string filtr_statis = cb_Status.SelectedValue.ToString();
+            //      infoGridStudents.ItemsSource = context.Student.
+            //          Where(i => i.studGroup == filtr_krs && i.status == filtr_statis).ToList();
+            //  }
+
+
+            //}
+            //if (cb_Status.SelectedItem != null)
+            //{
+            //    string filtr_statis = cb_Status.SelectedValue.ToString();
+            //    infoGridStudents.ItemsSource = context.Student.
+            //        Where(i => i.status == filtr_statis).ToList();
+            //    if (cb_Group.SelectedItem != null)
+            //    {
+            //        int filtr_gr = cb_Group.SelectedIndex + 1;
+            //        infoGridStudents.ItemsSource = context.Student.
+            //         Where(i => i.studGroup == filtr_gr && i.status == filtr_statis).ToList();
+            //    }
+            //    if (cb_Kurs.SelectedItem != null)
+            //    {
+            //        int filtr_krs = cb_Kurs.SelectedIndex + 1;
+            //        infoGridStudents.ItemsSource = context.Student.
+            //          Where(i => i.studGroup == filtr_krs && i.status == filtr_statis).ToList();
+            //    }
+
+
+            //}
+
+
+
+
+
+
+
         }
 
         private void Comboboxes()
@@ -88,7 +176,7 @@ namespace elj.frames
 
 
             var grp = context.StudGroup.ToList();
-            grp.Add("fgddfg");
+            //grp.Add();
 
 
             cb_Group.ItemsSource = grp;
